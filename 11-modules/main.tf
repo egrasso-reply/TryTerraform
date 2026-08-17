55092771300a2691579cace78692072d8f2cd587
Emanuele Grasso <e.grasso@goreply.io>
2026-08-17 17:57:29 +0200

    Capitolo 11: aggiunge modulo Cloud Foundation Fabric per il bucket
    Affianca al modulo locale ./modules/bucket un secondo modulo
    equivalente ma pubblico, mantenuto da Google (Fabric), per
    confrontare "fatto in casa" vs "maturo e versionato". Pinnato a
    v57.0.0, richiede rete verso GitHub in fase di init.

--- 11-modules/main.tf
+++ 11-modules/main.tf
@@ -14,3 +14,23 @@ module "demo_bucket" {
     managed_by  = "terraform"
   }
 }
+
+# Stesso identico obiettivo, ma con un modulo pubblico mantenuto da Google
+# (Cloud Foundation Fabric) invece che scritto a mano. Buono per mostrare
+# la differenza tra "modulo fatto in casa" e "modulo maturo, testato,
+# versionato" che si scarica da terraform init (richiede rete verso GitHub).
+#
+# ref pinnato a v57.0.0: verifica che sia ancora l'ultimo tag stabile su
+# https://github.com/GoogleCloudPlatform/cloud-foundation-fabric/tags
+# prima della lezione.
+module "demo_bucket_fabric" {
+  source     = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/gcs?ref=v57.0.0"
+  project_id = var.project_id
+  name       = "${var.project_id}-${var.environment}-fabric-demo"
+  location   = "EU"
+
+  labels = {
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+}
--- 11-modules/outputs.tf
+++ 11-modules/outputs.tf
@@ -5,3 +5,11 @@ output "bucket_name" {
 output "bucket_url" {
   value = module.demo_bucket.bucket_url
 }
+
+output "fabric_bucket_name" {
+  value = module.demo_bucket_fabric.name
+}
+
+output "fabric_bucket_url" {
+  value = module.demo_bucket_fabric.url
+}
